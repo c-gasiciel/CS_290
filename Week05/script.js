8bd443b4337d98e5608f8fa49ae0c79aa346ed5d
clarissa <clarissa.gasiciel@gmail.com>
2018-10-30 00:05:30 -0500

    Updated cell selection functions

--- Week05/script.js
+++ Week05/script.js
@@ -1,5 +1,13 @@
+/******************************************************************************
+CS 290:  Web Development
+Author: Clarissa Gasiciel
+Date Modified: October 29, 2018
+Description:  Javascript for DOM and Events assignment
+******************************************************************************/
+
 //Create table
 var newTable = document.createElement("table");
+newTable.style.border = "1px solid black";
 
 var newHeader = document.createElement("thead");
 
@@ -8,6 +16,7 @@ var headRow = document.createElement("tr");
 for(var i = 1; i <= 4; i++){
   var headerElement = document.createElement("th");
   headerElement.textContent = "Header " + i;
+  headerElement.style.border = "1px solid black";
   headRow.appendChild(headerElement);
 }
 
@@ -38,20 +47,18 @@ var col = 0;
 
 var tableBody = document.getElementsByTagName("tbody")[0];
 var navRow = tableBody.firstElementChild;
-console.log(navRow);
 
 var chosenCell = navRow.firstElementChild;
+chosenCell.style.border = "3px solid black";
 
 //Create directional buttons
 var upButton = document.createElement("button");
 upButton.textContent = "up";
 upButton.addEventListener("click", () => {
   //If not at top of the table, move selector up
-  if(row != 0){
-    row--;
-    navRow = navRow.previousElementSibling;
-    chosenCell = navRow.children[col];
-    chosenCell.style.border = "3px solid black";
+  if(row > 0){
+    chosenCell.style.border = "1px solid black";
+    return moveSelection("up");
   }
 });
 document.body.appendChild(upButton);
@@ -61,14 +68,11 @@ document.body.appendChild(upButton);
 var downButton = document.createElement("button");
 downButton.textContent = "down";
 downButton.addEventListener("click", () => {
-  //If not at top of the table, move selector up
-  if(row != 3){
-    row++;
-    navRow = navRow.nextElementSibling;
-    chosenCell = navRow.children[col];
-    chosenCell.style.border = "3px solid black";
-  }
-});
+  if(row < 2){
+    chosenCell.style.border = "1px solid black";
+    return moveSelection("down");
+    }
+  });
 
 document.body.appendChild(downButton);
 
@@ -76,25 +80,62 @@ document.body.appendChild(downButton);
 
 var leftButton = document.createElement("button");
 leftButton.textContent = "left";
+leftButton.addEventListener("click", () => {
+  if(col > 0){
+    chosenCell.style.border = "1px solid black";
+    return moveSelection("left");
+  }
+});
 document.body.appendChild(leftButton);
+
+
 
 var rightButton = document.createElement("button");
 rightButton.textContent = "right";
+rightButton.addEventListener("click", () => {
+  if(col < 3){
+    chosenCell.style.border = "1px solid black";
+    return moveSelection("right");
+  }
+});
 document.body.appendChild(rightButton);
+
+
 
 var space = document.createElement("p");
 document.body.appendChild(space);
 
-/*
-var selected = document.getElementsByTagName("td"[1][0]);
-selected.style.border "3px solid black";
 
 //Select cell based on directional button pressed
 function moveSelection (direction){
+  if(direction == "up"){
+    row--;
+    navRow = navRow.previousElementSibling;
+    chosenCell = navRow.children[col];
+    chosenCell.style.border = "3px solid black";
+  }
 
+  else if(direction == "down"){
+    //If not at top of the table, move selector up
+      row++;
+      navRow = navRow.nextElementSibling;
+      chosenCell = navRow.children[col];
+  }
+
+  else if(direction =="left"){
+    col--;
+    chosenCell = navRow.children[col];
+  }
+
+  else if(direction == "right"){
+    col++;
+    chosenCell = navRow.children[col];
+  }
+
+  chosenCell.style.border = "3px solid black";
 }
 
-*/
+
 //Create Mark Cell button
 var markCell = document.createElement("button");
 markCell.textContent = "Mark Cell";
@@ -102,11 +143,9 @@ markCell.addEventListener("click", changeBackground);
 document.body.appendChild(markCell);
 
 //Allow Mark Cell to change background color of selected cell
-function changeBackground(event){
-  var toChange = document.getElementsByTagName("td");
-
-  if(toChange.style.backgroundColor != "yellow"){
-    toChange.style.backgroundColor = "yellow";
+function changeBackground(){
+  if(chosenCell.style.backgroundColor != "yellow"){
+    chosenCell.style.backgroundColor = "yellow";
   }
 
 }
