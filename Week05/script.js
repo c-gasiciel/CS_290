2db589d2343c6b6eff97cba36118cdd56ecd20a1
clarissa <clarissa.gasiciel@gmail.com>
2018-10-29 23:32:27 -0500

    Added cell selection to table

--- Week05/script.js
+++ Week05/script.js
@@ -1,17 +1,17 @@
 //Create table
 var newTable = document.createElement("table");
 
-var newHeader = document.createElement("thead")
+var newHeader = document.createElement("thead");
 
-var newRow = document.createElement("tr");
+var headRow = document.createElement("tr");
 
 for(var i = 1; i <= 4; i++){
   var headerElement = document.createElement("th");
   headerElement.textContent = "Header " + i;
-  newRow.appendChild(headerElement);
+  headRow.appendChild(headerElement);
 }
 
-newHeader.appendChild(newRow);
+newHeader.appendChild(headRow);
 newTable.appendChild(newHeader);
 
 var newBody = document.createElement("tbody");
@@ -20,7 +20,7 @@ for(var j = 1; j < 4; j++){
   var newRow = document.createElement("tr");
 
   for(var k = 1; k <= 4; k++){
-    var elContent = document.createElement("td")
+    var elContent = document.createElement("td");
     elContent.textContent = j + ", " + k;
     elContent.style.border = "1px solid black";
     newRow.appendChild(elContent);
@@ -32,14 +32,47 @@ newTable.appendChild(newBody);
 
 document.body.appendChild(newTable);
 
+//Set up variables to access elements
+var row = 0;
+var col = 0;
+
+var tableBody = document.getElementsByTagName("tbody")[0];
+var navRow = tableBody.firstElementChild;
+console.log(navRow);
+
+var chosenCell = navRow.firstElementChild;
+
 //Create directional buttons
 var upButton = document.createElement("button");
 upButton.textContent = "up";
+upButton.addEventListener("click", () => {
+  //If not at top of the table, move selector up
+  if(row != 0){
+    row--;
+    navRow = navRow.previousElementSibling;
+    chosenCell = navRow.children[col];
+    chosenCell.style.border = "3px solid black";
+  }
+});
 document.body.appendChild(upButton);
+
+
 
 var downButton = document.createElement("button");
 downButton.textContent = "down";
+downButton.addEventListener("click", () => {
+  //If not at top of the table, move selector up
+  if(row != 3){
+    row++;
+    navRow = navRow.nextElementSibling;
+    chosenCell = navRow.children[col];
+    chosenCell.style.border = "3px solid black";
+  }
+});
+
 document.body.appendChild(downButton);
+
+
 
 var leftButton = document.createElement("button");
 leftButton.textContent = "left";
